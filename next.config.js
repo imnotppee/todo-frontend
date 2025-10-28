5291ad0d591fad4ef04cee088be54d4ad25b561b
imnotppee <67030142@kmitl.ac.th>
2025-10-29 03:24:00 +0700

    feat: Can use Todo website

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // output: 'export',
+  // basePath: '/todo-frontend',
+  // assetPrefix: '/todo-frontend/',
+  images: { unoptimized: true },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
+};
+
+console.log("✅ Loaded ENV from next.config.js:", process.env.NEXT_PUBLIC_API_URL);
+
+module.exports = nextConfig;
